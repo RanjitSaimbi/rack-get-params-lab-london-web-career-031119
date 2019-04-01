55be5b2e6a1da6a2ef2edc53a3905de97eaca363
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 12:42:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
       elsif req.path.match(/cart/)
-      @@cart.each {|item| resp.write "#{item}\n"}
+      # @@cart.each {|item| resp.write "#{item}\n"}
     else
       resp.write "Path Not Found"
     end
