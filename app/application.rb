7f86c4b5cc31de2a2eaa614656ad90a15cea96ad
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 12:52:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
         end 
       elsif req.path.match(/add/)
         item = req.params["item"]
-        if 
+        if @@items.include? item 
           
         else 
           
